7090a1b098738f1acaafb5c08bcf9485453a617b
gmanipon <gmanipon@jpl.nasa.gov>
2019-02-14 00:02:52 +0000

    remove imports not yet installed via install_requires

--- osaka/__init__.py
+++ osaka/__init__.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-from future import standard_library
-standard_library.install_aliases()
 __version__ = "0.0.1"
 __url__ = "http://gitlab.jpl.nasa.gov:8000/browser/trunk/HySDS/hysds"
 __description__ = "Osaka (Object Store Abstraction K Arcitecture)"
