c87ed4a5eb76d8b01811470ab363bfe658f7bc96
Gerald Manipon <pymonger@gmail.com>
2019-05-28 14:22:50 +0000

    bump version

--- osaka/__init__.py
+++ osaka/__init__.py
@@ -2,6 +2,6 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __url__ = "http://gitlab.jpl.nasa.gov:8000/browser/trunk/HySDS/hysds"
 __description__ = "Osaka (Object Store Abstraction K Arcitecture)"
