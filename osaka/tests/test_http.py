058c0b96639f0d450b51cefc6d896978bf278e36
Mike Cayanan <michael.d.cayanan@jpl.nasa.gov>
2024-03-18 10:16:04 -0700

    update URL

--- osaka/tests/test_http.py
+++ osaka/tests/test_http.py
@@ -4,7 +4,7 @@ import osaka.storage.http
 
 class StorageHTTPTest(unittest.TestCase):
     def test_isComposite_with_binary_file(self):
-        test_url = "http://landsat-pds.s3.amazonaws.com/scene_list.gz"
+        test_url = "https://landsat-pds.s3.amazonaws.com/c1/L8/scene_list.gz"
 
         storage_http = osaka.storage.http.HTTP()
         try:
